72d251ed6b50d761966f668239397bfde58f8518
TimE1211 <timmy121192@gmail.com>
2017-04-27 16:59:43 -0400

    initial commit

---
+++ High Voltage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  High Voltage
+//
+//  Created by Timothy Hang on 4/27/17.
+//  Copyright © 2017 Timothy Hang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
